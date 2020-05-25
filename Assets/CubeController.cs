332ad9a30d2147d390ca4d10c8537c18508d78fd
Masato Ogawa <mogawa@xevo.com>
2020-05-25 10:28:54 +0900

    Updated

---
+++ Assets/CubeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_rigidbody.AddForce(-5 * _rigidbody.velocity);
+        
+    }
+    void FixedUpdate() {
+        _rigidbody.AddForce(50 * _rigidbody.velocity);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.TransformDirection(Vector3.forward * speed);
+        transform.rotation = Quaternion.Euler(0,0,0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0.1f, 0);
+        }
+        // else if (Input.GetButtonDown("Left"))
+        // {
+        //     transform.position += new Vector3(0, 0.1f, 0);
+        //     Debug.Log("Left Key Pressed");
+        // }
+
+        
+#if UNITY_EDITOR
+        float rightLeftKey = Input.GetAxis("rightLeftKeyUnityEditor");
+#else
+        float rightLeftKey = Input.GetAxis("rightLeftKeyWebGLChrome");
+#endif
+        if (rightLeftKey < 0)
+        {
+            transform.position += new Vector3(-0.5f, 0, 0);
+            transform.rotation = Quaternion.Euler(0,0,30);
+        }
+        if (rightLeftKey > 0)
+        {
+            transform.position += new Vector3(0.5f, 0, 0);
+            transform.rotation = Quaternion.Euler(0,0,-30);
+        }
+        
+#if UNITY_EDITOR
+        float upDownKey = Input.GetAxis("upDownKeyUnityEditor");
+#else
+        float upDownKey = Input.GetAxis("upDownKeyWebGLChrome");
+#endif
+        if (upDownKey < 0)
+        {
+            transform.position += new Vector3(0, 0.5f, 0);
+            transform.rotation = Quaternion.Euler(-30,0,0);
+        }
+        if (upDownKey > 0)
+        {
+            transform.position += new Vector3(0, -0.5f, 0);
+            transform.rotation = Quaternion.Euler(30,0,0);
+        }
+    }
+
+}
