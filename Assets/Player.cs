bfa5b4e32cd9af27bfe21e2a1de1a1ae1cf0ddf0
Masato Ogawa <mogawa@xevo.com>
2020-05-25 10:41:36 +0900

    WASD supported

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -17,28 +17,17 @@ public class Player : MonoBehaviour
         transform.position += transform.TransformDirection(Vector3.forward * speed);
         transform.rotation = Quaternion.Euler(0,0,0);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += new Vector3(0, 0.1f, 0);
-        }
-        // else if (Input.GetButtonDown("Left"))
-        // {
-        //     transform.position += new Vector3(0, 0.1f, 0);
-        //     Debug.Log("Left Key Pressed");
-        // }
-
-        
 #if UNITY_EDITOR
         float rightLeftKey = Input.GetAxis("rightLeftKeyUnityEditor");
 #else
         float rightLeftKey = Input.GetAxis("rightLeftKeyWebGLChrome");
 #endif
-        if (rightLeftKey < 0)
+        if (rightLeftKey < 0 || Input.GetKey(KeyCode.A)) // Left
         {
             transform.position += new Vector3(-0.5f, 0, 0);
             transform.rotation = Quaternion.Euler(0,0,30);
         }
-        if (rightLeftKey > 0)
+        if (rightLeftKey > 0 || Input.GetKey(KeyCode.D)) // Right
         {
             transform.position += new Vector3(0.5f, 0, 0);
             transform.rotation = Quaternion.Euler(0,0,-30);
@@ -49,12 +38,12 @@ public class Player : MonoBehaviour
 #else
         float upDownKey = Input.GetAxis("upDownKeyWebGLChrome");
 #endif
-        if (upDownKey < 0)
+        if (upDownKey < 0 || Input.GetKey(KeyCode.W)) // Up
         {
             transform.position += new Vector3(0, 0.5f, 0);
             transform.rotation = Quaternion.Euler(-30,0,0);
         }
-        if (upDownKey > 0)
+        if (upDownKey > 0 || Input.GetKey(KeyCode.S)) // Down
         {
             transform.position += new Vector3(0, -0.5f, 0);
             transform.rotation = Quaternion.Euler(30,0,0);
